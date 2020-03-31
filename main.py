5e795db731414466be4ec680902428e7e9c1e4fb
Dominim Vllasa <dominim.vllasa@outlook.de>
2020-04-01 00:57:45 +0200

    - Add different Colors top polygone

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from enum import Enum
 from functools import partial
 
 from PySide2 import QtWidgets, QtGui, QtCore
-from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
+from PySide2.QtCore import SIGNAL, QObject
+from PySide2.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QPushButton
 from mainwindow import Ui_MainWindow
 from os import listdir
 import os
@@ -23,8 +24,7 @@ class PolygonAnnotation(QtWidgets.QGraphicsPolygonItem):
         self.setFlag(QtWidgets.QGraphicsItem.ItemSendsGeometryChanges, True)
 
         self.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.setBrush(QtGui.QColor(63, 136, 143, 100))
-
+        # self.setBrush(QtGui.QColor(63, 136, 143, 100))
 
         self.mItems = []
 
@@ -65,18 +65,29 @@ class PolygonAnnotation(QtWidgets.QGraphicsPolygonItem):
                 self.move_item(i, self.mapToScene(point))
         return super(PolygonAnnotation, self).itemChange(change, value)
 
-    def hoverEnterEvent(self, event):
-        self.setBrush(QtGui.QColor(63, 136, 143, 150))
-        super(PolygonAnnotation, self).hoverEnterEvent(event)
+    # def mousePressEvent(self, event):
+    #     print(self.mPoints)
+    #     labelType.append(self.mPoints)
+    #     # self.setBrush(QtGui.QColor(255, 0, 0, 150))
 
+    # def hoverEnterEvent(self, event):
+    #     self.setBrush(QtGui.QColor(63, 136, 143, 150))
+    #     super(PolygonAnnotation, self).hoverEnterEvent(event)
+    #
     # def hoverLeaveEvent(self, event):
     #     self.setBrush(QtGui.QBrush(QtCore.Qt.NoBrush))
     #     super(PolygonAnnotation, self).hoverLeaveEvent(event)
 
-
 class Instructions(Enum):
     NoInstruction = 0
     PolygonInstruction = 1
+    BackItem = 0
+    NextItem = 1
+    Red = 0
+    Yellow = 1
+    Green = 2
+    Blue = 3
+    Pink = 4
 
 
 class ImageWithPolygon:
@@ -104,9 +115,9 @@ class ImageScene(QGraphicsScene):
         self.setSceneRect(self.imageItem.boundingRect())
         self.imageName = filename
         if filename in imagePolygon:
-            imagPolyData = imagePolygon[filename]
-            self.allPolygonPointsFromImg = imagPolyData
-            for i in imagPolyData:
+            imagePolyData = imagePolygon[filename]
+            self.allPolygonPointsFromImg = imagePolyData
+            for i in imagePolyData:
                 self.polygonItem = PolygonAnnotation()
                 self.addItem(self.polygonItem)
                 self.polygonItems.append(self.polygonItem)
@@ -114,7 +125,7 @@ class ImageScene(QGraphicsScene):
                     print('VALUE', k)
                     self.positionAddPoint(k)
 
-    def setCurrentInstruction(self, instruction):
+    def setCurrentInstruction(self, instruction, colorcode):
         self.currentInstruction = instruction
         self.polygonItem = PolygonAnnotation()
         self.addItem(self.polygonItem)
@@ -122,6 +133,18 @@ class ImageScene(QGraphicsScene):
         if len(self.polygonPoints) != 0:
             self.allPolygonPointsFromImg.append(self.polygonPoints)
             self.polygonPoints = []
+        print(colorcode)
+        if colorcode == 0:
+            self.polygonItem.setBrush(QtGui.QColor(255, 0, 0, 150))
+        elif colorcode == 1:
+            self.polygonItem.setBrush(QtGui.QColor(255, 255, 0, 150))
+        elif colorcode == 2:
+            self.polygonItem.setBrush(QtGui.QColor(0, 255, 0, 150))
+        elif colorcode == 3:
+            self.polygonItem.setBrush(QtGui.QColor(0, 0, 255, 150))
+        elif colorcode == 4:
+            self.polygonItem.setBrush(QtGui.QColor(255, 0, 255, 150))
+
 
     def mousePressEvent(self, event):
         if self.currentInstruction == Instructions.PolygonInstruction:
@@ -133,6 +156,9 @@ class ImageScene(QGraphicsScene):
         self.polygonItem.addPoint(position)
         self.polygonItem.addPoint(position)
         self.polygonPoints.append(position)
+
+        # self.polygonItem.setBrush(QtGui.QColor(63, 136, 143, 150))
+       # print('POLY ', self.polygonItem.mItems)
 
     def mouseMoveEvent(self, event):
         if self.currentInstruction == Instructions.PolygonInstruction:
@@ -201,11 +227,6 @@ class ImageView(QtWidgets.QGraphicsPathItem):
         return super(ImageView, self).itemChange(change, value)
 
 
-class NextOrBack(Enum):
-    BackItem = 0
-    NextItem = 1
-
-
 class MainWindow(QMainWindow):
     factor = 2.0
 
@@ -216,46 +237,79 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowTitle("Truth Data generator Parcels")
         self.counterImages = 0
+        self.colorNum = 0
         self.ui.imageView.setRenderHints(QtGui.QPainter.Antialiasing | QtGui.QPainter.SmoothPixmapTransform)
         self.ui.imageView.setMouseTracking(True)
         self.mView = self.ui.imageView
         self.mScene = ImageScene(self)
         self.mView.setScene(self.mScene)
-        self.directory = ''
-        self.files = self.menuBar().addMenu("File").addAction("Open")
-        self.files.triggered.connect(self.getDirectory)
-
-        QtWidgets.QShortcut(QtGui.QKeySequence.ZoomIn, self.mView, self.zoomIn)
-        QtWidgets.QShortcut(QtGui.QKeySequence.ZoomOut, self.mView, self.zoomOut)
-
-        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Escape), self.mView,
-                            activated=partial(self.mScene.setCurrentInstruction, Instructions.NoInstruction))
-
-        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Y), self.mView,
-                            activated=partial(self.mScene.setCurrentInstruction, Instructions.NoInstruction))
-
-        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_X), self.mView, self.mScene.removePolygon)
-
-        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_A), self.mView,
-                            activated=partial(self.mScene.setCurrentInstruction, Instructions.PolygonInstruction))
-
-    def getDirectory(self):
-        self.directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Open directory",
-                                                                    QtCore.QStandardPaths.writableLocation(
-                                                                        QtCore.QStandardPaths.PicturesLocation))
+        self.directory = '/Users/dominim/Desktop/TestData'
         self.filenames = [f for f in listdir(self.directory) if isfile(join(os.path.realpath(self.directory), f))]
         self.realpathImages = []
         for index, filename in enumerate(self.filenames):
             self.realpathImages.append(self.directory + '/' + filename)
             self.ui.imageName.addItem(filename)
-        self.load_image(NextOrBack.BackItem)
-        self.ui.nextImageButton.clicked.connect(partial(self.load_image, NextOrBack.NextItem.value))
-        self.ui.backButton.clicked.connect(partial(self.load_image, NextOrBack.BackItem.value))
+        self.load_image(Instructions.BackItem)
+        self.ui.nextImageButton.clicked.connect(partial(self.load_image, Instructions.NextItem.value))
+        self.ui.backButton.clicked.connect(partial(self.load_image, Instructions.BackItem.value))
         self.ui.removeButton.clicked.connect(self.mScene.removePolygon)
         QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Right), self.mView,
-                            activated=partial(self.load_image, NextOrBack.NextItem.value))
+                            activated=partial(self.load_image, Instructions.NextItem.value))
         QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Left), self.mView,
-                            activated=partial(self.load_image, NextOrBack.BackItem.value))
+                            activated=partial(self.load_image, Instructions.BackItem.value))
+
+
+        # self.files = self.menuBar().addMenu("File").addAction("Open")
+        # self.files.triggered.connect(self.getDirectory)
+
+        self.ui.boxButton.setStyleSheet("color: #FF0000")
+        self.ui.bagButton.setStyleSheet("color: #FFFF00")
+        self.ui.transparentButton.setStyleSheet("color: #00FF00")
+        self.ui.unknownButton.setStyleSheet("color: #0000FF")
+        self.ui.noneButton.setStyleSheet("color: #FF00FF")
+
+        self.ui.boxButton.clicked.connect(partial(self.setColorCode, Instructions.Red.value))
+        self.ui.bagButton.clicked.connect(partial(self.setColorCode, Instructions.Yellow.value))
+        self.ui.transparentButton.clicked.connect(partial(self.setColorCode, Instructions.Green.value))
+        self.ui.unknownButton.clicked.connect(partial(self.setColorCode, Instructions.Blue.value))
+        self.ui.noneButton.clicked.connect(partial(self.setColorCode, Instructions.Pink.value))
+
+        QtWidgets.QShortcut(QtGui.QKeySequence.ZoomIn, self.mView, self.zoomIn)
+        QtWidgets.QShortcut(QtGui.QKeySequence.ZoomOut, self.mView, self.zoomOut)
+
+        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Escape), self.mView,
+                            activated=partial(self.mScene.setCurrentInstruction, Instructions.NoInstruction,
+                                              self.colorNum))
+
+        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_X), self.mView, self.mScene.removePolygon)
+
+        QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_A), self.mView,
+                            activated=partial(self.mScene.setCurrentInstruction, Instructions.PolygonInstruction,
+                                              self.colorNum))
+
+    def setColorCode(self, code):
+        self.colorNum = code
+        print(self.colorNum)
+        self.mScene.setCurrentInstruction(Instructions.PolygonInstruction, self.colorNum)
+
+
+    # def getDirectory(self):
+    #     self.directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Open directory",
+    #                                                                 QtCore.QStandardPaths.writableLocation(
+    #                                                                     QtCore.QStandardPaths.PicturesLocation))
+    #     self.filenames = [f for f in listdir(self.directory) if isfile(join(os.path.realpath(self.directory), f))]
+    #     self.realpathImages = []
+    #     for index, filename in enumerate(self.filenames):
+    #         self.realpathImages.append(self.directory + '/' + filename)
+    #         self.ui.imageName.addItem(filename)
+    #     self.load_image(Instructions.BackItem)
+    #     self.ui.nextImageButton.clicked.connect(partial(self.load_image, Instructions.NextItem.value))
+    #     self.ui.backButton.clicked.connect(partial(self.load_image, Instructions.BackItem.value))
+    #     self.ui.removeButton.clicked.connect(self.mScene.removePolygon)
+    #     QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Right), self.mView,
+    #                         activated=partial(self.load_image, Instructions.NextItem.value))
+    #     QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Left), self.mView,
+    #                         activated=partial(self.load_image, Instructions.BackItem.value))
 
     @QtCore.Slot()
     def zoomIn(self):
